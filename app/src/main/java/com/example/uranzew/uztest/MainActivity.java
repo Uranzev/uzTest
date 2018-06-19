633e0ea89f556731584cfe297c277b9e6e9153cc
Uranzev <uran_zew88@yahoo.com>
2018-06-19 16:29:15 +0800

    log out error fixed

--- app/src/main/java/com/example/uranzew/uztest/MainActivity.java
+++ app/src/main/java/com/example/uranzew/uztest/MainActivity.java
@@ -158,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
         if (callbackManager.onActivityResult(requestCode, resultCode, data)) {
             return;
